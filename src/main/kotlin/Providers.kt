82b106d5ff17b892c6b5eb7ec276c0b7a76ed15f
Ngonyoku <ngonyoku001@gmail.com>
2020-07-20 10:22:45 +0300

    Initial commit

---
+++ src/main/kotlin/Providers.kt
@@ -0,0 +1,57 @@
+import com.rsk.security.Argument.argument
+import java.security.Provider
+import java.security.Security
+
+class ProviderDetails(val providerName: String, val name: String)
+
+class Providers {
+
+    private val filter: String by argument()
+
+    fun run() {
+        listAllProviders()
+    }
+
+    private fun listAllProviders() {
+        if (filter.isEmpty()) {
+
+            getProviders().forEach {
+                display(it)
+            }
+        } else {
+            getFilteredProviders().forEach{ display("${it.providerName}: ${it.name}") }
+        }
+    }
+
+    private fun display(provider: Provider) {
+        println(provider.name)
+
+        println("------------------------------------------------------------------------------------------")
+
+        provider.entries.forEach { entry ->
+            println("\t ${entry.key}, ${entry.value}")
+        }
+
+        println("------------------------------------------------------------------------------------------")
+
+    }
+
+    private fun display(message: String) {
+        println(message)
+    }
+
+    private fun getProviders(): List<Provider> {
+        val providers = Security.getProviders()
+        val list: List<Provider> = providers.asList()
+        return list
+    }
+
+    private fun getFilteredProviders(): List<ProviderDetails> {
+        return Security.getProviders().flatMap { provider ->
+            provider.entries
+                    .filter { it -> it.key.toString().contains(filter, true) }
+                    .map { ProviderDetails(provider.name, it.key.toString()) }
+        }
+    }
+
+}
---
+++ src/main/kotlin/SecurityTools.kt
@@ -0,0 +1,70 @@
+package com.rsk
+
+import Providers
+import com.rsk.security.*
+
+
+fun main(args: Array<String>) {
+
+
+    val ishelp = if (args.isNotEmpty()) args[0] else "";
+
+    if (args.size < 2 || ishelp == "--help") {
+        println("usage: java SecurityTools --help:hash|sign|providers")
+
+        println("hashing: java SecurityToolsKt [-op 'hash'] [-f filename]  [-d destfilename] [-p provider] [-a algorithm] [-o] [-encode]")
+        println("signing: java SecurityToolsKt  [-op 'sign'] -s [-f filename] [-d signaturefile] [-p provider] [-a algorithm]")
+        println("providers: java SecurityToolsKt  [-op 'providers']  [-f filename]  [-d destfilename] [-p provider] [-a algorithm] [-o] [-encode]")
+        return
+    }
+
+    if (ishelp.startsWith("--help:")) {
+        val helpon = ishelp.split(":")[1]
+        when (helpon) {
+            // help on each part
+        }
+        return
+    }
+
+    // all the flags that can be used
+    ParseArgs.setupDefaultValues(
+            arrayOf(ArgumentInitializers("operation", ArgumentType.StringType(), "-op"),
+                    ArgumentInitializers("algorithm", ArgumentType.StringType(), "-a"),
+                    ArgumentInitializers("keystoreType", ArgumentType.StringType("JKS"), "-keystoretype"),
+                    ArgumentInitializers("encode", ArgumentType.BooleanType(true), "-encode"),
+                    ArgumentInitializers("sign", ArgumentType.BooleanType(true), "-s"),
+                    ArgumentInitializers("verify", ArgumentType.BooleanType(), "-v"),
+                    ArgumentInitializers("keyStoreFilename", ArgumentType.StringType(), "-keystore"),
+                    ArgumentInitializers("keypass", ArgumentType.StringType(), "-keypass"),
+                    ArgumentInitializers("keyStorePass", ArgumentType.StringType(), "-keystorepass"),
+                    ArgumentInitializers("keyAlias", ArgumentType.StringType(), "-alias"),
+                    ArgumentInitializers("provider", ArgumentType.StringType(), "-p"),
+                    ArgumentInitializers("fileName", ArgumentType.StringType(), "-f"),
+                    ArgumentInitializers("destFileName", ArgumentType.StringType(), "-d"),
+                    ArgumentInitializers("signatureFileName", ArgumentType.StringType(), "-sigfilename"),
+                    ArgumentInitializers("overwrite", ArgumentType.BooleanType(), "-o"),
+                    ArgumentInitializers("decode", ArgumentType.BooleanType(), "-decode"),
+                    ArgumentInitializers("filter", ArgumentType.StringType(), "-filter")
+            ))
+
+
+    ParseArgs(args)
+
+    val type: ArgumentType.StringType = ParseArgs.arguments.get("operation")!!.type as ArgumentType.StringType
+
+    // execute the code
+    when (type.value.toLowerCase()) {
+        "hash" -> {
+        }
+
+        "sign" -> {
+        }
+
+        "providers" -> {
+            Providers().run()
+        }
+    }
+}
+
+
+
