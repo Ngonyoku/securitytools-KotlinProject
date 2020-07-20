d5973a341ee7c6e033ce9092d96de5eb1980b985
Ngonyoku <ngonyoku001@gmail.com>
2020-07-20 10:54:23 +0300

    ProvidersDetails Class Added

--- src/main/kotlin/Providers.kt
+++ src/main/kotlin/Providers.kt
@@ -1,15 +1,29 @@
+import com.rsk.security.Argument
+import com.rsk.security.Argument.argument
 import java.security.Provider
 import java.security.Security
 
+class ProviderDetails(val providerName: String, val name: String)
+
 class Providers {
+
+    private val filter: String by argument()
+
     fun run() {
         listAllProviders()
     }
 
     private fun listAllProviders() {
-        getProviders().forEach {
-            display(it)
+        if (filter.isEmpty()) {
+            getProviders().forEach {
+                display(it)
+            }
+        } else {
+            getFilteredProviders().forEach {
+                println(println("${it.providerName} : ${it.name}"))
+            }
         }
+
     }
 
     private fun display(provider: Provider) {
@@ -27,5 +41,14 @@ class Providers {
         val providers = Security.getProviders()
         val list: List<Provider> = providers.asList()
         return list
+    }
+
+    private fun getFilteredProviders(): List<ProviderDetails> {
+        return Security.getProviders()
+                .flatMap { provider ->
+                    provider.entries
+                            .filter { it -> it.key.toString().contains(filter, true) }
+                            .map { ProviderDetails(provider.name, it.key.toString()) }
+                }
     }
 }
